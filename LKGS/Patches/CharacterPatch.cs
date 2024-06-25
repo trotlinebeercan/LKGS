244828e86443fc60c5c815e9ada277cc2725e3be
trotlinebeercan <hopper.dustin@gmail.com>
2024-06-24 19:21:22 -0500

    CharacterPatch: Use proper configuration for application of patch

--- LKGS/Patches/CharacterPatch.cs
+++ LKGS/Patches/CharacterPatch.cs
@@ -4,37 +4,64 @@ namespace LKGS;
 
 public class CharacterPatch : IPatch
 {
+    private string bEnableInfiniteEnergyId = "bEnableInfiniteEnergy";
+    private string bEnableInfiniteHealthId = "bEnableInfiniteHealth";
+    private string bAllowNextDayPenaltiesId = "bAllowNextDayPenalties";
+
     public void Initialize()
     {
-        // throw new System.NotImplementedException();
+        ConfigManager.Instance.StartSection("Character Management")
+            .Create(bEnableInfiniteEnergyId, "Infinite Energy", false,
+                "Prevents Energy depletion when using any tool.",
+                null,
+                new ConfigurationManagerAttributes {}
+            )
+            .Create(bEnableInfiniteEnergyId, "Infinite Health", false,
+                "Prevents Health depletion when taking any damage.",
+                null,
+                new ConfigurationManagerAttributes {}
+            )
+            .Create(bAllowNextDayPenaltiesId, "Allow Next Day Penalties", true,
+                "Staying up too late or depleting all Energy will cause a penalty on the next day.",
+                null,
+                new ConfigurationManagerAttributes {}
+            )
+        .EndSection("Character Management");
     }
 
-    public void OnTriggerUpdate()
+    private bool AllowPenalties()
     {
-        // throw new System.NotImplementedException();
+        return ConfigManager.Instance.GetValue<bool>(bAllowNextDayPenaltiesId);
+    }
+
+    private void AdjustStatImpl(PlayerStat statToAdjust, ref float amount)
+    {
+        if (statToAdjust == PlayerStat.Energy)
+            amount = ConfigManager.Instance.GetValue<bool>(bEnableInfiniteEnergyId) ? 0.0f : amount;
+        if (statToAdjust == PlayerStat.Health)
+            amount = ConfigManager.Instance.GetValue<bool>(bEnableInfiniteHealthId) ? 0.0f : amount;
     }
 
     [HarmonyLib.HarmonyPatch(typeof(ScPlayerStats), nameof(ScPlayerStats.ExhaustedPenalty))]
     [HarmonyLib.HarmonyPrefix]
     public static bool ExhaustedPenalty()
     {
-        // don't let the exhausted penalty be set
-        return false;
+        // don't let the exhausted penalty be set if we're allowing penalties
+        return Plugin.GetStoredPatch<CharacterPatch>().AllowPenalties();
     }
 
     [HarmonyLib.HarmonyPatch(typeof(ScPlayerStats), nameof(ScPlayerStats.LateNightPenalty))]
     [HarmonyLib.HarmonyPrefix]
     public static bool LateNightPenalty()
     {
-        // don't let the late night penalty be set
-        return false;
+        // don't let the late night penalty be set if we're allowing penalties
+        return Plugin.GetStoredPatch<CharacterPatch>().AllowPenalties();
     }
 
     [HarmonyLib.HarmonyPatch(typeof(ScPlayerStats), nameof(ScPlayerStats.AdjustStat))]
     [HarmonyLib.HarmonyPrefix]
     public static void AdjustStat(PlayerStat statToAdjust, ref float amount)
     {
-        if (statToAdjust == PlayerStat.Energy) amount = 0.0f; // inf stamina
-        if (statToAdjust == PlayerStat.Health) amount = 0.0f; // god mode
+        Plugin.GetStoredPatch<CharacterPatch>().AdjustStatImpl(statToAdjust, ref amount);
     }
 }
