a5d3cfb403c554a917a355fdca47c17674f0a8c0
trotlinebeercan <hopper.dustin@gmail.com>
2024-07-03 15:58:42 -0500

    LKGS: Global 'remove unused usings'

--- LKGS/Plugin.cs
+++ LKGS/Plugin.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-
-using BC = BepInEx.Configuration;
 using HL = HarmonyLib;
 using SM = UnityEngine.SceneManagement;
 
